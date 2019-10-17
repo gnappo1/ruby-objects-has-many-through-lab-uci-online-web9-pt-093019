b0d4bbbd6c539d0286aa1aed24091492faf99f5b
Matteo Piccini <gnappo.1@live.it>
2019-10-17 02:30:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,8 +1,25 @@
 class Patient 
-  def initialize
-    
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
   end
   
+  def self.all
+    @@all
+  end
   
+  def appointments
+    Appointment.all.select {|a| a.patient == self}
+  end
   
+  def doctors
+    appointments.map(&:doctor)
+  end
+  
+  def new_appointment(name, genre)
+    #Appointment.new(name, self, genre)
+  end
 end 
